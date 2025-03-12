0d306523652a0630a2c2f46ebb1b8f0d59fa5bd2
Nilina Nilaksha <nilina610@gmail.com>
2025-03-13 01:10:39 +0530

    dash-1

---
+++ backend/models/customer.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+const customerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+    address: {
+      mainAddress: {
+        type: String,
+        required: true,
+      },
+      invoiceAddress: {
+        type: String,
+        required: true,
+      },
+    },
+    email: {
+      mainEmail: {
+        type: String,
+        required: true,
+      },
+      invoiceEmail: {
+        type: String,
+        required: true,
+      },
+    },
+    companySize: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Customer = mongoose.model("Customer", customerSchema);
+export default Customer;
