31c0dba9709c383c4260a32899b1e5986af3867b
Nisal2522 <nisalamarasekara@gmail.com>
2025-03-22 11:42:34 +0530

    Update Profile.jsx

--- frontend/src/pages/food/DepartmentHead/Profile.jsx
+++ frontend/src/pages/food/DepartmentHead/Profile.jsx
@@ -302,7 +302,7 @@ const ProfileHeadFood = () => {
                 <svg className="w-6 h-6 mr-2 text-white font-poppins" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M21 12.79l-4.8-1.9m0 0L17 8l-7 7 5 5 6-7.21z"></path>
                 </svg>
-                <p><strong>Last Update:</strong> {formattedLastUpdate} ({lastUpdateAgo})</p>
+                <p class="text-white"><strong>Last Update:</strong> {formattedLastUpdate} ({lastUpdateAgo})</p>
               </div>
 
               <div className="flex items-center mb-2">
