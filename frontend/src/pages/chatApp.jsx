5611dbba93d62972d987252b6717335113c24fd6
Nilina Nilaksha <nilina610@gmail.com>
2025-04-29 18:46:20 +0530

    update till cuNo

---
+++ frontend/src/pages/chatApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const chatApp = () => {
+    return (
+        <div className="flex justify-center items-center h-screen text-5xl">
+            To be implemented
+            <div className='animate-spin'>
+
+
+                😊
+            </div>
+        </div>
+    )
+}
+
+export default chatApp
